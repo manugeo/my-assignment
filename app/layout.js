797172bf7215b87ba6a7e4af8c4c9cb4f93a3699
Manu <manugeo13@gmail.com>
2024-03-19 09:19:09 +0530

    Initial Commit.

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="min-h-screen bg-slate-50">
+          <div className='max-w-screen-sm mx-auto'>
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,6 @@
+import Landing from "@/components/pages/landing";
+import Image from "next/image";
+
+export default function Page() {
+  return <Landing />
+}
