526f9d79f2368cc7270f5f3bbb5d1b1f40283d01
Manu <manugeo13@gmail.com>
2024-03-20 00:01:37 +0530

    Installed chart.js

--- components/pages/dashboard.jsx
+++ components/pages/dashboard.jsx
@@ -4,6 +4,46 @@ import { useEffect } from "react"
 import { TextLead } from "../ui/texts"
 import { useAuth } from "@/context/authContext"
 import { useRouter } from "next/navigation"
+import { Bar } from "react-chartjs-2"
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from "chart.js"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const barChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+}
+
+const barChartData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [28, 48, 40, 19, 86, 27, 90],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+}
 
 const Dashboard = () => {
   const router = useRouter()
@@ -18,6 +58,8 @@ const Dashboard = () => {
   return (
     <div className="pb-6 flex flex-col">
       <TextLead className="mt-6 mx-6 text-center">Dashboard Page</TextLead>
+
+      <Bar data={barChartData} options={barChartOptions} />
     </div>
   )
 }
