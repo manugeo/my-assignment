713a52dc92d11455bea7596b89d19281f4020f9c
Manu <manugeo13@gmail.com>
2024-03-19 23:41:49 +0530

    Set up 'Logout' button.

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,13 @@
+'use client'
+
 import Link from "next/link"
 import MainNav from "./main-nav"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
+import { useAuth } from "@/context/authContext"
 
 const Header = () => {
+  const { currentUser, logoutUser } = useAuth()
   return (
     <div className="w-full h-16 px-4 bg-slate-200 flex items-center">
       <Link href='/'>
@@ -13,17 +17,17 @@ const Header = () => {
         </Avatar>
       </Link>
 
-      <MainNav className="hidden md:flex mx-4" />
+      <MainNav className=" hidden md:flex mx-4" />
 
-      <Button className="ml-auto" asChild>
+      <Button className={`${currentUser ? 'hidden' : ''} ml-auto`} asChild>
         <Link href="/login">Login</Link>
       </Button>
 
-      <Button variant="secondary" className="ml-4">
+      <Button variant="secondary" className={`${currentUser ? 'hidden' : ''} ml-4`}>
         <Link href="/register">Register</Link>
       </Button>
 
-      <Button className="hidden ml-4">Logout</Button>
+      <Button className={`${currentUser ? '' : 'hidden'} ml-auto`} onClick={logoutUser}>Logout</Button>
     </div>
   )
 }
