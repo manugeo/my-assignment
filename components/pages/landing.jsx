6f22390522a83649d2088aac625979358f46d601
Manu <manugeo13@gmail.com>
2024-03-20 18:21:29 +0530

    Minor updates to the landing page.

--- components/pages/landing.jsx
+++ components/pages/landing.jsx
@@ -1,9 +1,11 @@
-import { TextLead } from "../ui/texts"
+import { TextH4, TextP } from "../ui/texts"
 
 const Landing = () => {
   return (
     <div className="pb-6 flex flex-col">
-      <TextLead className="mt-6 mx-6 text-center">Landing Page</TextLead>
+      <TextH4 className="mt-6 mx-6 text-center">Log in to Access Dashboard</TextH4>
+
+      <TextP className="mt-6 mx-6 text-center">{"Please log in to access the dashboard page. You can use the email 'eve.holt@reqres.in' to log in."}</TextP>
     </div>
   )
 }
