d7e9361024edb338e86f99aed027082fa735ccfa
Manu <manugeo13@gmail.com>
2024-03-20 17:00:55 +0530

    Added new Charity Category chart.

---
+++ components/charity-category-chart.jsx
@@ -0,0 +1,65 @@
+import { Doughnut } from "react-chartjs-2"
+import {
+  ArcElement,
+  Chart as ChartJS,
+  Legend,
+  Title,
+  Tooltip
+} from "chart.js"
+import { useCallback, useEffect, useState } from "react"
+import { Skeleton } from "./ui/skeleton"
+import { formatNumber } from "@/lib/utils"
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title
+)
+
+const CharityCategoryChart = ({ data, className, ...props }) => {
+  console.log({ data });
+  const [chartData, setChartData] = useState(null)
+  const [totalCharity, setTotalCharity] = useState(null)
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: `Charity Expenditure by Category${totalCharity ? ` (Total: $${formatNumber(totalCharity)})` : ''}`,
+      }
+    }
+  }
+
+
+  const prepareChartData = useCallback(() => {
+    const chartData = {
+      labels: ['Category 1', 'Category 2', 'Category 3'],
+      datasets: [
+        {
+          data,
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }
+      ]
+    }
+    setChartData(chartData)
+    setTotalCharity((data && (data.length === 3)) ? data.reduce((a, b) => a + b, 0) : null)
+  }, [data])
+
+  useEffect(() => {
+    prepareChartData()
+  }, [prepareChartData])
+
+  return (
+    (!data || !data.length || !chartData)
+      ? <Skeleton className="w-full h-60 mt-6" {...props} />
+      : <Doughnut data={chartData} options={options} className={className} />
+  )
+}
+
+export default CharityCategoryChart
--- components/pages/dashboard.jsx
+++ components/pages/dashboard.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation"
 import useData from "@/hooks/useData"
 import RevenueProfitChart from "../revenue-profit-chart"
 import PageButtons from "../page-buttons"
+import CharityCategoryChart from "../charity-category-chart"
 
 const Dashboard = () => {
   const router = useRouter()
@@ -15,18 +16,22 @@ const Dashboard = () => {
   const [currentPage, setCurrentPage] = useState(null)
   const [totalPages, setTotalPages] = useState(null)
   const [dataToDisplay, setDataToDisplay] = useState([])
+  const [charityDataToDisplay, setCharityDataToDisplay] = useState(null)
 
   useEffect(() => {
     if (!data && !data.length) {
       setCurrentPage(null)
       setTotalPages(null)
       setDataToDisplay([])
+      setCharityDataToDisplay(null)
       return
     }
-
     setCurrentPage(1)
     setTotalPages(Math.ceil(data.length / 12))
-    setDataToDisplay(data.slice(0, 12))
+    const newDataToDisplay = data.slice(0, 12)
+    setDataToDisplay(newDataToDisplay)
+    const newCharityDataToDisplay = calculateCharityData(newDataToDisplay)
+    setCharityDataToDisplay(newCharityDataToDisplay)
   }, [data])
 
   useEffect(() => {
@@ -39,17 +44,44 @@ const Dashboard = () => {
     if (change === 'next') {
       if (currentPage < totalPages) {
         const nextPage = currentPage + 1
-        setDataToDisplay(data.slice((nextPage - 1) * 12, nextPage * 12))
+        const newDataToDisplay = data.slice((nextPage - 1) * 12, nextPage * 12)
+        setDataToDisplay(newDataToDisplay)
+        setCharityDataToDisplay(calculateCharityData(newDataToDisplay))
         setCurrentPage(nextPage)
       }
     }
     else if (change === 'previous') {
       if (currentPage > 1) {
         const previousPage = currentPage - 1
-        setDataToDisplay(data.slice((previousPage - 1) * 12, previousPage * 12))
+        const newDataToDisplay = data.slice((previousPage - 1) * 12, previousPage * 12)
+        setDataToDisplay(newDataToDisplay)
+        setCharityDataToDisplay(calculateCharityData(newDataToDisplay))
         setCurrentPage(previousPage)
       }
     }
+  }
+
+  const calculateCharityData = (displayData) => {
+    if (!displayData || !displayData.length) return null
+    const charityData = [0, 0, 0]
+    for (let i = 0; i < displayData.length; i++) {
+      const charityCategory = displayData[i].charity.category
+      const charityFund = displayData[i].charity.fund
+      switch (charityCategory) {
+        case 'Category 1':
+          charityData[0] += charityFund
+          break;
+        case 'Category 2':
+          charityData[1] += charityFund
+          break;
+        case 'Category 3':
+          charityData[2] += charityFund
+          break;
+        default:
+          break;
+      }
+    }
+    return charityData
   }
 
   const maxRevenue = useMemo(() => {
@@ -59,15 +91,16 @@ const Dashboard = () => {
         maxRevenue = data[i].revenue
       }
     }
-    console.log({ maxRevenue });
-
     return maxRevenue
   }, [data])
 
   return (
     <div className="px-6 pb-6 flex flex-col">
       <TextH3 className="mt-6">Finance</TextH3>
+
       <RevenueProfitChart className="mt-6" data={dataToDisplay} maxRevenue={maxRevenue} />
+
+      <CharityCategoryChart className="mt-6 max-h-[300px]" data={charityDataToDisplay} />
 
       {(currentPage && totalPages)
         ? <PageButtons totalPages={totalPages} currentPage={currentPage} className="mt-6"
