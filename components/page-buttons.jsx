4fa4cdcf3b983e7d5d0e562a83b1204e06a08b41
Manu <manugeo13@gmail.com>
2024-03-20 15:17:52 +0530

    Dashboard - Added pagination buttons.

---
+++ components/page-buttons.jsx
@@ -0,0 +1,25 @@
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "./ui/pagination"
+import { TextSmall } from "./ui/texts"
+
+
+const PageButtons = ({ totalPages = 10, currentPage = 1, onNext = () => { }, onPrevious = () => { } }) => {
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious className={`${(currentPage === 1) ? 'pointer-events-none' : ''}`} role="button" onClick={onPrevious} />
+        </PaginationItem>
+
+        <PaginationItem>
+          <TextSmall>{`${currentPage} of ${totalPages}`}</TextSmall>
+        </PaginationItem>
+
+        <PaginationItem>
+          <PaginationNext className={`${(currentPage === totalPages) ? 'pointer-events-none' : ''}`} role="button" onClick={onNext} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
+
+export default PageButtons
--- components/pages/dashboard.jsx
+++ components/pages/dashboard.jsx
@@ -6,14 +6,29 @@ import { useAuth } from "@/context/authContext"
 import { useRouter } from "next/navigation"
 import useData from "@/hooks/useData"
 import RevenueProfitChart from "../revenue-profit-chart"
+import PageButtons from "../page-buttons"
 
 const Dashboard = () => {
   const router = useRouter()
   const { currentUser } = useAuth()
   const { data } = useData()
+
+  console.log(data);
+
+  const [currentPage, setCurrentPage] = useState(null)
+  const [totalPages, setTotalPages] = useState(null)
   const [dataToDisplay, setDataToDisplay] = useState([])
 
   useEffect(() => {
+    if (!data && !data.length) {
+      setCurrentPage(null)
+      setTotalPages(null) 
+      setDataToDisplay([])
+      return
+    }
+
+    setCurrentPage(1)
+    setTotalPages(Math.ceil(data.length / 12))
     setDataToDisplay(data.slice(0, 12))
   }, [data])
 
@@ -23,10 +38,30 @@ const Dashboard = () => {
     }
   }, [currentUser, router])
 
+  const handlePageChange = (change = 'next') => {
+    if (change === 'next') {
+      if (currentPage < totalPages) {
+        const nextPage = currentPage + 1
+        setDataToDisplay(data.slice((nextPage - 1) * 12, nextPage * 12))
+        setCurrentPage(nextPage)
+      }
+    }
+    else if (change === 'previous') {
+      if (currentPage > 1) {
+        const previousPage = currentPage - 1
+        setDataToDisplay(data.slice((previousPage - 1) * 12, previousPage * 12))
+        setCurrentPage(previousPage)
+      }
+    }
+  }
+
   return (
     <div className="px-6 pb-6 flex flex-col">
       <TextH3 className="mt-6">Finance</TextH3>
       <RevenueProfitChart data={dataToDisplay} />
+
+      {(currentPage && totalPages) && <PageButtons totalPages={totalPages} currentPage={currentPage}
+        onNext={() => handlePageChange('next')} onPrevious={() => handlePageChange('previous')} />}
     </div>
   )
 }
